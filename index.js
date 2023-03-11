74e6dc1caa72d462e2ea00c205ac0637f887f2f0
Юлия <jullka5@mail.ru>
2023-03-11 17:06:20 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+/* eslint-disable no-debugger */
+const func = () => {
+    const sum = (a, b) => a + b;
+    let x = 10;
+    let y = 20;
+    
+    console.log(sum(x, y)); // 30
+    debugger;
+    x = 20;
+    y = 30;
+    
+    console.log(sum(x, y)); // 50
+    };
+    
+    func();
